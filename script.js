5da6171c6fcbe31fa1ec1c1789e6680af2557c30
Azeez Wariz <warizazeez05@gmail.com>
2022-11-16 13:37:11 +0100

    string project

---
+++ script.js
@@ -0,0 +1,32 @@
+let statement = "A long time ago in a country far away from Nigeria...";
+var wordlen=statement.length;
+console.log(wordlen)
+
+console.log(statement.slice(0, 1));
+console.log(statement.substring(0, 1));
+console.log(statement.substr(0, 1));
+
+console.log(statement.slice(12, 16));
+console.log(statement.substring(12, 16));
+console.log(statement.substr(12, 3));
+
+console.log(statement.slice(20, 28));
+console.log(statement.substring(20, 28));
+console.log(statement.substr(20, 8));
+
+console.log(statement.slice(32, 37));
+console.log(statement.substring(32, 37));
+console.log(statement.substr(32, 5));
+
+
+console.log(statement.slice(42, 50));
+console.log(statement.substring(42, 50));
+console.log(statement.substr(42, 8));
+
+console.log(statement.slice());
+console.log(statement.slice());
+console.log(statement.slice());
+
+
+
+
